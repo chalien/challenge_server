214ca19d40c970b1df3c181b69a0a04d22d55813
Charly Palencia <ingcharlypalencia@gmail.com>
2016-07-21 23:33:52 -0500

    Validate Household fields

--- test/models/household_test.rb
+++ test/models/household_test.rb
@@ -4,4 +4,10 @@ describe Household do
   let(:household) { Household.new }
   should have_many(:people)
   should have_many(:vehicles).through(:people)
+
+  should validate_presence_of(:address);
+  should validate_presence_of(:zip);
+  should validate_presence_of(:city);
+  should validate_presence_of(:state);
+  should validate_presence_of(:number_of_bedrooms);
 end
